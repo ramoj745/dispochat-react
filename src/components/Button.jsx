49855ddfa71f1314ba8b22b0a7a64f1cf112957e
Ramoj <ramoj745@gmail.com>
2024-12-09 21:35:53 +0800

    Delete initial files

--- src/components/Button.jsx
+++
@@ -1,9 +0,0 @@
-import React, { useState } from "react"; //useState for animations/routing?
-import "./Button.css"
-
-function Button(props) {
-
-  return <button onClick={props.onClick} style={props.style}>{props.children}</button>;
-}
-
-export default Button;
--- src/components/Listing.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import "./Listing.css";
-
-function Listing(props) {
-  return <div className="listing">{props.children}</div>;
-}
-
-export default Listing;
