eff2e18377a904178643e5cdc758eb233acec085
Ramoj <ramoj745@gmail.com>
2024-12-13 04:43:01 +0800

    Add ref for input submission thru icon button

--- client/src/components/ChatInput.jsx
+++ client/src/components/ChatInput.jsx
@@ -1,8 +1,23 @@
-import React from "react";
+import React, { useState, forwardRef } from "react";
 import "./ChatInput.css";
 
-function ChatInput() {
-  return <input className="chat-input" placeholder="Enter your message here"></input>;
-}
+const ChatInput = forwardRef(function ChatInput(props, ref) {
+  const [input, setInput] = useState("");
+
+  function onEventOfChange(event) {
+    setInput(event.target.value);
+  }
+
+  return (
+    <input
+      type="text"
+      ref={ref}
+      onChange={onEventOfChange}
+      value={input}
+      className="chat-input"
+      placeholder="Enter your message here"
+    ></input>
+  );
+});
 
 export default ChatInput;
