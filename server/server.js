0f66a9a5aa443fa0fc363564c3bc25872d647900
Ramoj <ramoj745@gmail.com>
2024-12-13 04:50:54 +0800

    Add load, new, join event listeners and /sendMessage route

--- server/server.js
+++ server/server.js
@@ -7,7 +7,6 @@ import "dotenv/config";
 import cors from "cors";
 import Message from "./messageSchema.js";
 import User from "./userSchema.js";
-import message from "./messageSchema.js";
 
 const app = express();
 const port = 3000;
@@ -36,6 +35,23 @@ app.use(express.json());
 io.on("connection", (socket) => {
   console.log("New client connected: ", socket.id);
   emitRooms();
+
+  socket.on("joinRoom", async (roomId) => { //listen if client joined a room
+    socket.join(roomId);
+    console.log("Joined Room", roomId)
+
+    const messages = await Message.find({ room: roomId }).sort({ createdAt: 1 }); // on join, load messages from db
+    socket.emit("loadMessages", messages)
+
+  });
+
+  socket.on("newMessage", async (data) => { //listen for any new messages, then emit to specific roomId
+    io.to(data.roomId).emit("newMessage", {
+      roomId: data.roomId,
+      senderId: data.senderId,
+      message: data.message,
+    });
+  });
 
   socket.on("disconnect", () => {
     console.log("Client disconnected.", socket.id);
@@ -79,7 +95,7 @@ app.post("/createRoom", async (req, res) => {
       res.status(201).json(newRoom);
       console.log("Room Created!");
     } else {
-      res.json({message: "Error: User already exists"})
+      res.json({ message: "Error: User already exists" });
     }
   } catch (err) {
     console.error(err);
@@ -88,18 +104,32 @@ app.post("/createRoom", async (req, res) => {
 
 app.post("/getRoom", async (req, res) => {
   const roomId = req.body.roomId;
-  //socket can be get here also
+  const clientId = req.body.socket; //socket id passed from client
 
   const room = await ChatRoom.findOne({ _id: roomId });
 
   if (!room) {
     return res.status(404).json({ message: "Room not found (404)" });
   } else {
-    room.users.push(socket.id);
-    res
-      .status(200)
-      .json({ message: `User {socket.id} joined the room.`, room });
+    room.users.push(clientId);
+    await room.save();
+    res.status(200).json({ message: `User ${clientId} joined the room.`, room });
   }
+});
+
+app.post("/sendMessage", async (req, res) => {
+  const { roomId, senderId, message } = req.body;
+
+  const newMessage = new Message({
+    user: senderId,
+    room: roomId,
+    message: message,
+  })
+
+  await newMessage.save()
+
+  res.status(200).json({message: "Request went through"})
+
 });
 
 server.listen(port, () => {
