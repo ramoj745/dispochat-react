ce6095aaa8b06cadcbff256e690fd0dbc407f759
Ramoj <ramoj745@gmail.com>
2024-12-18 19:44:13 +0800

    Delete reference for userSchema, and delete userSchema

--- server/chatRoomSchema.js
+++ server/chatRoomSchema.js
@@ -4,7 +4,7 @@ const chatRoomSchema = new mongoose.Schema({
   name: { type: String, required: true },
   password: { type: String },
   isPasswordProtected: { type: Boolean, default: false },
-  users: [{ type: String, ref: 'User' }],
+  users: [{ type: String }],
 });
 
 const chatRoom = mongoose.model("ChatRoom", chatRoomSchema);
